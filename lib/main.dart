292358eb7cd7d905df7fbe393d92aa60ead7219f
aniket.jain <aniketjain8441@gmail.com>
2023-07-11 13:13:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:gamify_app/pages/home_pages.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: HomePage(),
+    );
+  }
+}
